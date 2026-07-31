9519f86881bbbda20edc7777d65bbc89cea220be
scariotluizier1507-rgb <scariotluizier1507@gmail.com>
2026-07-31 02:05:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-alert("Script carregado!");
-
 const botao = document.querySelector("button");
 
 botao.addEventListener("click", function () {
 
-    alert("Botão funcionando!");
+    const usuario = document.querySelector('input[type="text"]').value;
+    const senha = document.querySelector('input[type="password"]').value;
+
+    if (usuario === "admin" && senha === "1234") {
+
+        alert("Bem-vindo ao TREINACCGL!");
+
+        window.location.href = "admin.html";
+
+    } else {
+
+        alert("Usuário ou senha incorretos.");
+
+    }
 
 });
