fc84f48633f8d2150c28d400ec0dfe8ac8fe8d48
scariotluizier1507-rgb <scariotluizier1507@gmail.com>
2026-07-31 01:40:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const botao = document.querySelector("button");
+const botao = document.querySelector(“button”);
 
-botao.addEventListener("click", function () {
+botao.addEventListener(“click”, function () {
 
-    const usuario = document.querySelector('input[type="text"]').value;
-    const senha = document.querySelector('input[type="password"]').value;
-
-    if (usuario === "admin" && senha === "1234") {
-        alert("Bem-vindo ao TREINACCGL!");
-    } else {
-        alert("Usuário ou senha incorretos.");
-    }
+const usuario = document.querySelector('input[type="text"]').value;
+const senha = document.querySelector('input[type="password"]').value;
+if (usuario === "admin" && senha === "1234") {
+    alert("Bem-vindo ao TREINACCGL!");
+    // abre a tela do administrador
+    window.location.href = "admin.html";
+} else {
+    alert("Usuário ou senha incorretos.");
+}
 
 });
